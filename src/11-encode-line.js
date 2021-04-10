32a73d1e90c32c7aa6b7a88169e0beb5127ecf73
Evgeniya Karabanova <karabanovaep@yandex.ru>
2021-04-11 00:41:35 +0300

    feat: add completed 11-encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const arrRes = [];
+  const result = {};
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    if (result[a] !== undefined) {
+      result[a]++;
+    } else {
+      result[a] = 1;
+    }
+  }
+  Object.entries(result).forEach(([key, value]) => {
+    arrRes.push(key);
+    arrRes.push(value);
+  });
+  return arrRes.join('');
 }
 
 module.exports = encodeLine;
